e323ce098e8d9e897e5b7f5c06384c62de50e6dc
kegby <cae.gonzalez.1988@gmail.com>
2022-09-24 23:13:49 +0000

    Updated init-script with correct yaml file and updated namespace yaml file

--- init-script.sh
+++ init-script.sh
@@ -15,15 +15,16 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 
 # ------ Untested from now on ----->
 # Launch Argocd App
-kubectl apply -f argocd-python.yaml
-sleep 60
+kubectl apply -f argocd-nginx.yaml
+sleep 80
 
 # Display Info to enter Argocd Server
 kubectl get services -n argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+# Check External Ip From Load Balancer Service. Open Browser, enter Ip.
+# Advanced config and proceed. Username = admin. Password = kubectl -n argocd get secret Output
 
 
 # Next Would be commands to download argocd , login and autosing. 
 # At this time will be done manually. 
-# Check External Ip From Load Balancer Service. Open Browser, enter Ip.
-# Advanced config and proceed. Username = admin. Password = kubectl -n argocd get secret Output
+# To install argocd it is complicated as first have to download brew. Then complete a couple of commands that takes long.
